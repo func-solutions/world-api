9a58bc2a103942808e65478ed69d7eafde4a2d95
Царюк Артём <42806772+funcid@users.noreply.github.com>
2022-09-14 02:07:46 +0300

    Больше утилит, bump 1.0.4

--- api/src/main/kotlin/me/func/world/utils.kt
+++ api/src/main/kotlin/me/func/world/utils.kt
@@ -1,7 +1,28 @@
 package me.func.world
 
+import net.minecraft.server.v1_12_R1.BlockPosition
+import net.minecraft.server.v1_12_R1.Blocks
+import net.minecraft.server.v1_12_R1.IBlockData
 import org.bukkit.Location
 import org.bukkit.World
+import ru.cristalix.core.formatting.Color
 import ru.cristalix.core.math.V3
 
 fun V3.toLocation(world: World) = Location(world, x, y, z)
+
+fun Location.orientation(): Orientation {
+    if (yaw < 0) yaw += 360f
+    if (yaw >= -45 && yaw <= 45) return Orientation.PX
+    if (yaw in 45.0..135.0) return Orientation.PY
+    return if (yaw in 135.0..225.0) Orientation.MX else Orientation.MY
+}
+
+fun Location.position() = BlockPosition(x, y, z)
+
+fun applyColor(data: IBlockData, color: Color?): IBlockData {
+    val block = data.block
+    if (color == null)
+        return data
+    // concrete и concrete_powder меняют цвет
+    return if (block === Blocks.dR || block === Blocks.dS || block === Blocks.STAINED_GLASS) block.fromLegacyData(color.woolData) else data
+}
--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 
 allprojects {
     group = "me.func"
-    version = "1.0.3"
+    version = "1.0.4"
 }
 
 subprojects {
